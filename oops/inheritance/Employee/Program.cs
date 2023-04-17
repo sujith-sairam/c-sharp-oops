8f58d59132f8bd98a3d7c04fdceb4d211b947cdc
sujith-sairam <89727004+sujith-sairam@users.noreply.github.com>
2023-04-17 15:12:13 +0530

    Add files via upload

---
+++ oops/inheritance/Employee/Program.cs
@@ -0,0 +1,72 @@
+﻿/*Create a class called Employee with attributes name, id, and salary.Then, 
+create two subclasses called Manager and Engineer that add attributes specific to each role,such
+as department for Manager and language for Engineer. Make sure that the Employee class has a 
+method calculate_salary() that can be overridden by the subclasses to calculate the employee's total salary.*/
+
+namespace Inheritance{
+//base class declaration
+class Employee{
+    public string name;
+    public int id;
+    protected double salary;
+    public double Salary{
+        get{ return salary; }
+        set { salary = value;}
+    }
+     public Employee(string name,int id,double salary){
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    //base salary method for employee
+    public void calculate_salary(){
+        Console.WriteLine(salary);
+    }
+}
+
+//Manager class inheriting Employee
+class Manager : Employee{
+    public string department;
+    public Manager(string name,int id,double salary,string department) : base(name,id,salary){
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+        this.department = department;
+    }
+
+    //salary method for manager
+     public void calculate_salary(double manager_salary){
+        Console.WriteLine(manager_salary);
+    }
+}
+
+//Engineer class inheriting Employee 
+class Engineer : Employee{
+    public string language;
+    public Engineer(string name,int id,double salary,string language) : base(name,id,salary){
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+        this.language = language;
+    }
+
+    //salary method for engineer
+     public void calculate_salary(double engineer_salary){
+        Console.WriteLine(engineer_salary);
+    }
+}
+class MainClass{
+    static void Main(string[] args) {
+    
+    //creating object for Manager
+    Manager manager = new Manager("Shyam",23,100000,"Sales");
+    manager.calculate_salary(manager.Salary);
+
+    //creating object for Enginner   
+    Engineer engineer = new Engineer("Raghu",12,50000,"python");
+    engineer.calculate_salary(engineer.Salary);
+    }
+}
+
+}
---
+++ oops/inheritance/Vehicle/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace FourWheelers{
+
+//base class declaration
+class Vehicle{
+    private string make="";
+    private string model="";
+    private int year;
+    private string color="";
+
+    public  string Make{ 
+        get {return make;}
+        set {make = value;}
+        }
+    public  string Model{ 
+        get {return model;}
+        set {model = value;}
+        }
+
+    public int Year{ 
+        get {return year;}
+        set {year = value;}
+        }
+
+    public  string Color{ 
+        get {return color;}
+        set {color = value;}
+        }
+
+    public Vehicle(string make,string model,int year,string color){
+        this.Make = make;
+        this.Model = model;
+        this.Year = year;
+        this.Color = color;
+    }
+     public void start_engine(){
+        Console.WriteLine($"The engine has been started...\nBrand:{Make}\nModel:{Model}\nYear:{Year}\nColor:{Color}");
+    }
+}
+    // Inheriting Car class from Vehicle
+    class Car : Vehicle{
+       private int num_doors;
+       public Car(string Make,string Model,int Year,string Color,int num_doors) : base(Make,Model,Year,Color){
+        this.Make = Make;
+        this.Model = Model;
+        this.Year = Year;
+        this.Color = Color;
+        this.num_doors = num_doors;
+       }
+
+    new public void start_engine(){
+        Console.WriteLine($"\nCar\nThe engine has been started...\nBrand:{Make}\nModel:{Model}\nYear:{Year}\nColor:{Color}\nDoors:{num_doors}");
+    }
+}
+
+// Inheriting Truck class from Vehicle
+  class Truck : Vehicle{
+       private int payload_capacity;
+       public Truck(string Make,string Model,int Year,string Color,int payload_capacity) : base(Make,Model,Year,Color){
+        this.Make = Make;
+        this.Model = Model;
+        this.Year = Year;
+        this.Color = Color;
+        this.payload_capacity = payload_capacity;
+       }
+
+    new public void start_engine(){
+        Console.WriteLine($"\nTruck\nThe engine has been started...\nBrand:{Make}\nModel:{Model}\nYear:{Year}\nColor:{Color}\nDoors:{payload_capacity}");
+    }
+}
+class MainClass{
+    public static void Main(string[] args){
+        //Creating object for car
+       Car car = new Car("Ford","r220",2022,"white",4);
+       car.start_engine();
+
+       //Creating object for truck
+       Truck truck = new Truck("Eicher","wkms123",2002,"black",10);
+       truck.start_engine();
+    }
+}
+
+}
